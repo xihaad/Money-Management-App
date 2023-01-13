75f5c922664fe27f608786effa5d294c0a66a61d
xihaad <zihad.rafi16@gmail.com>
2023-01-13 18:48:35 +0600

    ExpenseList show

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,8 +4,6 @@ import { GlobalStyles } from '../../constants/styles';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
-
-
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
@@ -36,6 +34,30 @@ const DUMMY_EXPENSES = [
     description: 'Another book',
     amount: 18.59,
     date: new Date('2022-02-18')
+  },
+  {
+    id: 'e6',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2022-01-05')
+  },
+  {
+    id: 'e7',
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01')
+  },
+  {
+    id: 'e8',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19')
+  },
+  {
+    id: 'e9',
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2022-02-18')
   }
 ];
 
@@ -53,7 +75,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700
   }
 });
