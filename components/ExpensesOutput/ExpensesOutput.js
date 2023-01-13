0a5e146ba70b04a627086bbf74067b65241b5900
xihaad <zihad.rafi16@gmail.com>
2023-01-13 23:28:17 +0600

    new modal added

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -61,6 +61,7 @@ const DUMMY_EXPENSES = [
   }
 ];
 
+
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -8,7 +8,7 @@ function ExpensesSummary({ expenses, periodName }) {
     return sum + expense.amount;
   }, 0);
 
-  
+
   return (
     <View style={styles.container}>
       <Text style={styles.period}>{periodName}</Text>
